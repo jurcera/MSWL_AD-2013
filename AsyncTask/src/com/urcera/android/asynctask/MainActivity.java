18d6fe1d67c0265aaf11a2124d9e69191b5168c6
Jesus Urcera <jurcera@gmail.com>
2013-06-03 18:18:10 +0200

    Realizada limpieza de log y toast de control y debug

--- AsyncTask/src/com/urcera/android/asynctask/MainActivity.java
+++ AsyncTask/src/com/urcera/android/asynctask/MainActivity.java
@@ -17,6 +17,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.webkit.MimeTypeMap;
 import android.widget.Toast;
@@ -33,20 +34,14 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	
 	    dialog = new ProgressDialog(this);
-	    dialog.setMessage("Descargando...");
 	    dialog.setTitle("Progreso");
+	    dialog.setMessage("Descargando...");
 	    dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
 	    dialog.setCancelable(false);
-	    //Realizamos cualquier otra operación necesaria
-	    //Creamos una nueva instancia y llamamos al método ejecutar
-	    //pasándole el string.
-	    
-	    
-	    
-	    urlFilename = "http://dl.dropboxusercontent.com/u/34909625/";
-	    filename = "rodrigo.jpg";
-	    	    
-	    urlConexion = urlFilename + filename;
+   
+	    urlFilename = "http://dl.dropboxusercontent.com/u/34909625/"; 	// Ruta de la imagen a descargar
+	    filename = "andrea.jpg";			// rodrigo.jpg y jesusadrian.jpg (Fichero a descargar)
+	    urlConexion = urlFilename + filename;							// URL completa
 	    URL miUrl = null;
 	    
 		try {
@@ -59,32 +54,27 @@ public class MainActivity extends Activity {
    
 	}
 	
-	//https://dl.dropboxusercontent.com/u/34909625/andrea.jpg
 	 
 	private class MiTarea extends AsyncTask<URL, Float, Integer>{
 
+		
 		protected void onPreExecute() {
 			dialog.setProgress(0);
 			dialog.setMax(100);
 			dialog.show(); //Mostramos el diálogo antes de comenzar
 		}
 
+		
 		protected Integer doInBackground(URL... urls) {
 			
-			Log.d("SUSO URL", String.valueOf(urls[0]));
 			int totalSize = 0;
 			int downloadedSize = 0;
 			
 			try {
-				 
-				Log.d("SUSO", "TRY");
-				
+			
 				//primero especificaremos el origen de nuestro archivo a descargar utilizando
 				
 				URL url = urls[0];				
-				//URL url = new URL(myUrl);
-				
-				Log.d("SUSO", "new url");
 				
 				URLConnection connection;
 				connection = url.openConnection();
@@ -100,7 +90,7 @@ public class MainActivity extends Activity {
 					
 					totalSize = urlConnection.getContentLength();
 					downloadedSize = 0;
-					//creamos un buffer y una variable para ir almacenando el tamaño temporal de este
+					// Creamos un buffer y una variable para ir almacenando el tamaño temporal de éste
 					byte[] buffer = new byte[1024];
 					int bufferLength = 0; 
 					
@@ -108,104 +98,48 @@ public class MainActivity extends Activity {
 					while ( (bufferLength = inputStream.read(buffer)) > 0 ) {
 						fileOutput.write(buffer, 0, bufferLength);
 						downloadedSize += bufferLength;
-						//podríamos utilizar una función para ir actualizando el progreso de lo
-						//actualizaProgreso(downloadedSize, totalSize);
-						publishProgress(((downloadedSize * 100) / (float) totalSize)); //Actualizamos los valores
-						Log.d("SUSO", String.valueOf((downloadedSize * 100) / totalSize));
+						// Actualizamos el progreso de la descarga
+						publishProgress(((downloadedSize * 100) / (float) totalSize)); 
 					}
 					fileOutput.close();
 			
 				} else {
 					Log.d("SUSO","Error de conexión");
 				}
-				/*
-				urlConnection.setRequestMethod("GET");
-				urlConnection.setDoOutput(true);
-				urlConnection.connect();
-				
-				Log.d("SUSO", "urlconection: " + urlConnection.getResponseMessage());
-				
-				Log.d("SUSO", "Connect");
-				
-				File SDCardRoot = Environment.getExternalStorageDirectory();
-				File file = new File(SDCardRoot,"test_andrea.jpg");
-				
-				Log.d("SUSO", "SDcard");
-
-				FileOutputStream fileOutput = new FileOutputStream(file);
-				 
-				Log.d("SUSO", "fileoutput");
-				
-				InputStream inputStream = urlConnection.getInputStream();
-				
-				Log.d("SUSO", ">> inputstream");
-				
-				int totalSize = urlConnection.getContentLength();
-				
-				Log.d("SUSO", "totalsize: " + String.valueOf(totalSize));
-				int downloadedSize = 0;
-				//creamos un buffer y una variable para ir almacenando el
-				//tamaño temporal de este
-				byte[] buffer = new byte[1024];
-				int bufferLength = 0; 
-				Log.d("SUSO", "Total size = " + String.valueOf(totalSize));
-				
-				//ahora iremos recorriendo el buffer para escribir el archivo de destino
-				while ( (bufferLength = inputStream.read(buffer)) > 0 ) {
-					fileOutput.write(buffer, 0, bufferLength);
-					downloadedSize += bufferLength;
-					//podríamos utilizar una función para ir actualizando el progreso de lo
-					//actualizaProgreso(downloadedSize, totalSize);
-					publishProgress(((downloadedSize * 100) / (float) totalSize)); //Actualizamos los valores
-					Log.d("SUSO", String.valueOf((downloadedSize * 100) / totalSize));
-				}
-				fileOutput.close();
-				*/
 						
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (Exception e) {
-				Toast.makeText(getApplicationContext(), 
-						"Problemas de conexión.\n    Reintentar "+e, Toast.LENGTH_SHORT)
-						.show();
 				e.printStackTrace();
 			}
 			
-			/*
-			Simularemos que descargamos un fichero
-			mediante un sleep
-		
-			for (int i = 0; i < 250; i++) {
-				//Simulamos cierto retraso
-				try {Thread.sleep(200); }
-				catch (InterruptedException e) {}
-
-				//publishProgress(i/250f); //Actualizamos los valores
-			}
-			*/
-
 			if(downloadedSize==totalSize){
 				return 1;
 			} else {
 				return 0;
 			}
 		}
+		
 
 		protected void onProgressUpdate (Float... valores) {
 			int p = Math.round(valores[0]);
 			dialog.setProgress(p);
 		}
+		
 
 		protected void onPostExecute(Integer bytes) {
 			dialog.dismiss();
 			int descarga = bytes;
 			File SDCardRoot = Environment.getExternalStorageDirectory();
 			File file = new File(SDCardRoot,filename);
-			//Ejecuta el archivo descargado
+			// Muestra el archivo descargado
 		
 			if (descarga == 1){
+				Toast toast = Toast.makeText(getApplicationContext(),"Descarga correcta", Toast.LENGTH_SHORT);
+				toast.setGravity(Gravity.TOP, 0, 0);
+				toast.show();
 				Intent intent = new Intent();
 				intent.setAction(android.content.Intent.ACTION_VIEW);
 				MimeTypeMap mime = MimeTypeMap.getSingleton();
@@ -213,9 +147,9 @@ public class MainActivity extends Activity {
 				String type = mime.getMimeTypeFromExtension(ext);
 				intent.setDataAndType(Uri.fromFile(file),type);
 				startActivity(intent);
-			}else
-				Toast.makeText(getApplicationContext(),	"Error en la descarga.", Toast.LENGTH_SHORT).show();
-			
+			} else {
+				Toast.makeText(getApplicationContext(),	"Error en la descarga", Toast.LENGTH_SHORT).show();
+			}
 		}
 	}
 	
