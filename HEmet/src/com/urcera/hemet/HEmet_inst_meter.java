8c2de3e0515a5d57ea472982b88d8ef567fd982e
Jesus Urcera <jurcera@gmail.com>
2013-06-21 12:43:12 +0200

    Antes de bluetooth

--- HEmet/src/com/urcera/hemet/HEmet_inst_meter.java
+++ HEmet/src/com/urcera/hemet/HEmet_inst_meter.java
@@ -1,16 +1,23 @@
 package com.urcera.hemet;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
-import com.androidplot.series.XYSeries;
-import com.androidplot.xy.LineAndPointFormatter;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYPlot;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PointLabelFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.XYStepMode;
+
 
 public class HEmet_inst_meter extends Activity {
 	
@@ -23,25 +30,75 @@ public class HEmet_inst_meter extends Activity {
 		
 		// initialize our XYPlot reference:
 		mySimpleXYPlot = (XYPlot) findViewById(R.id.mySimpleXYPlotVolt);
+		
+		Number[] voltageValues = {232.4, 232.8, 233.0, 232.9, 232.5, 233, 233, 233.1, 232.9, 232.8};
+		printGraph(getString(R.string.title_volt1), voltageValues, getString(R.string.label_time),getString(R.string.unit_volt));
+		
+		Button bt1 = (Button) this.findViewById(R.id.btVolt);	// Asignamos el boton1 a bt1
+		if (bt1 != null)										
+		{
+			bt1.setOnClickListener(new OnClickListener() { 		// Crea un listener para en boton1 	
+				
+				@Override
+				public void onClick(View v) {					// Llama a actividad1 al pulsar boton1
+										
+					Number[] voltageValues = {232.4, 232.8, 233.0, 232.9, 232.5, 233, 233, 233.1, 232.9, 232.8};
+					printGraph(getString(R.string.title_volt1), voltageValues, getString(R.string.label_time),getString(R.string.unit_volt));
+				}
+			} );
+		}
+		
+		Button bt2 = (Button) this.findViewById(R.id.btCurr);	// Asignamos el boton1 a bt1
+		if (bt2 != null)										
+		{
+			bt2.setOnClickListener(new OnClickListener() { 		// Crea un listener para en boton1 	
+				
+				@Override
+				public void onClick(View v) {					// Llama a actividad1 al pulsar boton1
+										
+					Number[] currentValues = {3.2, 3.8, 3.0, 2.9, 2.5, 3, 3, 3.1, 2.9, 2.8};
+					printGraph(getString(R.string.title_curr1), currentValues, getString(R.string.label_time),getString(R.string.unit_ampere));
+				}
+			} );
+		}
+		
+		Button bt3 = (Button) this.findViewById(R.id.btFreq);	// Asignamos el boton1 a bt1
+		if (bt3 != null)										
+		{
+			bt3.setOnClickListener(new OnClickListener() { 		// Crea un listener para en boton1 	
+				
+				@Override
+				public void onClick(View v) {					// Llama a actividad1 al pulsar boton1
+										
+					Number[] frequencyValues = {50, 49.83, 50.0, 50.9, 50.5, 51, 50.95, 50.1, 50.9, 50.8};
+					printGraph(getString(R.string.title_freq1), frequencyValues, getString(R.string.label_time),getString(R.string.unit_hertz));
+				}
+			} );
+		}
+		
+		
+	}
+	
+	public void printGraph(String tit, Number[] series1Numbers, String xLabel, String yLabel) {
 
-		// Create a couple arrays of y-values to plot:
-		Number[] series1Numbers = {232, 230, 231, 234, 237, 234};
-		//Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
+		mySimpleXYPlot.clear();
 
 		// Turn the above arrays into XYSeries':
 		XYSeries series1 = new SimpleXYSeries(
 				Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
 				SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
-				"Voltage");                             // Set the display title of the series
+				"");                             		// Set the display title of the series "Voltage"
 
 		// same as above
 		//XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Current");
+
 
 		// Create a formatter to use for drawing a series using LineAndPointRenderer:
 		LineAndPointFormatter series1Format = new LineAndPointFormatter(
 				Color.rgb(0, 200, 0),                   // line color
 				Color.rgb(0, 100, 0),                   // point color
-				null);                                  // fill color (none)
+				null,
+				new PointLabelFormatter(Color.WHITE));                                  // fill color (none)
 
 		// add a new series' to the xyplot:
 		mySimpleXYPlot.addSeries(series1, series1Format);
@@ -50,11 +107,20 @@ public class HEmet_inst_meter extends Activity {
 		//mySimpleXYPlot.addSeries(series2,	new LineAndPointFormatter(Color.rgb(0, 0, 200), Color.rgb(0, 0, 100), null));
 
 		// reduce the number of range labels
-		mySimpleXYPlot.setTicksPerRangeLabel(3);
+		mySimpleXYPlot.setTicksPerRangeLabel(2);
+		mySimpleXYPlot.setTicksPerDomainLabel(1);
+		mySimpleXYPlot.setTitle(tit);
+		mySimpleXYPlot.setDomainLabel(xLabel);
+		mySimpleXYPlot.setRangeLabel(yLabel);
 
-		// by default, AndroidPlot displays developer guides to aid in laying out your plot.
-		// To get rid of them call disableAllMarkup():
-		mySimpleXYPlot.disableAllMarkup();
+		// draw a domain line for every element plotted on the domain:
+		mySimpleXYPlot.setDomainStep(XYStepMode.INCREMENT_BY_VAL, 1);
+
+		// get rid of the decimal place on the display:
+		mySimpleXYPlot.setDomainValueFormat(new DecimalFormat("#"));
+		
+		
+		mySimpleXYPlot.redraw();
 
 	}
 
